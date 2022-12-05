84c18661f4df22d26922a7c5fee961eebe41e56c
inanakalin <111204187+inanakalin@users.noreply.github.com>
2022-12-06 00:39:02 +0300

    Day 4 - Homework 5

---
+++ Dictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using Dictionary;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dictionary<int, StudentData> students = new Dictionary<int, StudentData>();
+        StudentData student1 = new StudentData();
+        student1.Name = "İnan";
+        student1.Surname = "Akalın";
+        student1.Class = 11;
+
+        StudentData student2 = new StudentData() { Class = 5, Name = "Nazlı", Surname= "Akalın"};
+        StudentData student3 = new StudentData() { Name = "Ali", Surname = "Veli", Class = 10 };
+        students.Add(1, student3);
+        students.Add(2, student2);  
+        students.Add(3, student1);
+
+        Console.WriteLine(students[1].Name + " " + students[1].Surname + " " + students[1].Class);
+
+
+
+    }
+}
---
+++ Dictionary/StudentData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dictionary;
+
+namespace Dictionary
+{
+    class StudentData
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Class { get; set; }
+    }
+}
